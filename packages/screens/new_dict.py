13238d19cb4e6dd6b511890f7284e75001c28109
Selina Ernst <sernst2001@gmail.com>
2026-04-28 05:46:48 +0200

    last small update (cant use name of existing dict)

--- packages/screens/new_dict.py
+++ packages/screens/new_dict.py
@@ -9,14 +9,21 @@ from packages.kivymd_modules import (
 class DictionaryNew(MyScreen):
     dict_name=StringProperty('')
 
+
+    def __exists(self,dict_name:str=None):
+        from main import ChD
+        app=ChD.get_running_app()
+        dict_name=self.dict_name if dict_name==None else dict_name
+        return os.path.isdir(app.get_setting('dict_directory')+dict_name)
+
     def check_name(self):
         entry=(self.ids.name_entry.text)
         non_valid_names=set(["",None," "])
-        if entry not in non_valid_names:
+        if entry not in non_valid_names and not self.__exists(dict_name=entry):
             self.dict_name=entry
             return True
-        else:
-            return False
+        else: return False
+
     def confirm(self):
         is_name=self.check_name()
         if is_name: self.continue_on()
